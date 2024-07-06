49a0d8a031187f4a7e4db515336e737638ce7666
Takiul ISlam Sumon <takiulislam20@gmail.com>
2024-07-06 20:07:10 +0600

    Weather Bar

--- lib/Resultbar.dart
+++ lib/Resultbar.dart
@@ -1,9 +1,46 @@
-// import 'package:flutter/material.dart';
-// class resultbar extends StatelessWidget {
-//   const resultbar({super.key});
+import 'package:flutter/material.dart';
 
-//   @override
-//   Widget Context context) {
-//     return  ;
-//   }
-// }
+class resultBar extends StatelessWidget {
+  const resultBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      fallbackHeight: 150,
+      child: SingleChildScrollView(
+        child: Row(
+          children: [
+            SizedBox(
+              height: 150,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '03:00',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 8),
+                          Icon(Icons.cloud),
+                          SizedBox(height: 8),
+                          Text('302')
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/weather_screan.dart
+++ lib/weather_screan.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:weather_app/Resultbar.dart';
 
 class WeatherScrean extends StatelessWidget {
   const WeatherScrean({super.key});
@@ -27,72 +28,74 @@ class WeatherScrean extends StatelessWidget {
         ],
       ),
       body: Padding(
-          padding:  EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child:  Card(
-                  elevation: 10,
-                  child: Placeholder(
-                    strokeWidth: 0,color: Colors.black45,
-                    
-                     fallbackHeight: double.infinity,
-                     fallbackWidth: double.infinity,
-                     child: SizedBox(
-                       
-                       width: double.infinity,
-                       child: Card(
-                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
-                         child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          
-                           child: BackdropFilter(filter: ImageFilter.blur(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                elevation: 10,
+                child: Placeholder(
+                  strokeWidth: 0,
+                  color: Colors.black45,
+                  fallbackHeight: double.infinity,
+                  fallbackWidth: double.infinity,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Card(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5))),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(
                             sigmaX: 10,
                             sigmaY: 10,
-                            
-                           ),
+                          ),
                           //  blendMode: BlendMode.color,
-                             child:const Padding(
-                               padding:  EdgeInsets.all(16.0),
-                               child: Column(
-                                 children: [
-                                   
-                                   Text(
-                                     '300.006F',
-                                     style: TextStyle(fontSize: 32),
-                                   ),
-                                   SizedBox(height: 30),
-                                   Icon(Icons.cloud, size: 40),
-                                   SizedBox(height: 30),
-                                   Text(
-                                     'Rain',
-                                     style: TextStyle(fontSize: 24),
-                                     
-                                   )
-                                 ],
-                               ),
-                             ),
-                           ),
-                         ),
-                       ),
-                     ),
-                   ),
+                          child: Column(
+                            children: [
+                              Text(
+                                '300.006F',
+                                style: TextStyle(fontSize: 32),
+                              ),
+                              SizedBox(height: 30),
+                              Icon(Icons.cloud, size: 40),
+                              SizedBox(height: 30),
+                              Text(
+                                'Rain',
+                                style: TextStyle(fontSize: 24),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              Placeholder(
-                fallbackHeight: 150,
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Weather Forcast',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Placeholder(
-                fallbackHeight: 100,
-              )
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            resultBar(),
+            SizedBox(
+              height: 20,
+            ),
+            Placeholder(
+              fallbackHeight: 100,
+            )
+          ],
         ),
-      
+      ),
     );
   }
 }
